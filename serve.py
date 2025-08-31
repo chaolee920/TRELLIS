801d94e224cb3b42ce06cfe14125704c39da3dc1
mac <mac@MacMini-KJS-1.local>
2025-09-01 01:51:47 +0700

    restore: text23D

--- serve.py
+++ serve.py
@@ -36,7 +36,7 @@ args = get_args()
 app = FastAPI()
 
 # pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
-pipeline = TrellisTextTo3DPipeline.from_pretrained("microsoft/TRELLIS-text-xlarge")
+pipeline = TrellisTextTo3DPipeline.from_pretrained("/workspace/vol_sub17/models/TRELLIS-text-xlarge")
 pipeline.cuda()
 
 # t2i_pipe = DiffusionPipeline.from_pretrained("Tencent-Hunyuan/HunyuanDiT-v1.1-Diffusers-Distilled", torch_dtype=torch.float16).to("cuda")
