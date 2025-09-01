c629ba663573c683dc67b04ba1db29d60cdf5e91
mac <mac@MacMini-KJS-1.local>
2025-09-02 02:15:31 +0700

    add stf3 test

--- t2i23-test-schnell.py
+++ t2i23-test-schnell.py
@@ -14,7 +14,7 @@ import requests
 torch.cuda.empty_cache()
 
 # t2i_pipe = DiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5").to("cuda")
-t2i_pipe = DiffusionPipeline.from_pretrained("Keffisor21/flux1-schnell-bnb-nf4").cuda()
+t2i_pipe = DiffusionPipeline.from_pretrained("Keffisor21/flux1-schnell-bnb-nf4").to("cuda")
 
 pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
 pipeline.cuda()
