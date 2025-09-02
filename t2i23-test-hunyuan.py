4a7e0197ebe433a72b88ca4bcc4face0e794ec2f
mac <mac@MacMini-KJS-1.local>
2025-09-03 04:09:12 +0700

    fix

--- t2i23-test-hunyuan.py
+++ t2i23-test-hunyuan.py
@@ -25,9 +25,9 @@ t2i_pipe = HunyuanDiTPipeline.from_pretrained(
     dtype=torch.float16
 ).to("cuda")
 
-t2i_pipe.unet = t2i_pipe.unet.half()
-t2i_pipe.vae = t2i_pipe.vae.half()
-t2i_pipe.text_encoder = t2i_pipe.text_encoder.half()
+# t2i_pipe.transformer = t2i_pipe.transformer.half()
+# t2i_pipe.vae = t2i_pipe.vae.half()
+# t2i_pipe.text_encoder = t2i_pipe.text_encoder.half()
 
 i23_pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
 i23_pipeline.cuda()
