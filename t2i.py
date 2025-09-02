34e1c89658b69f69aba66fc2e5c63d2ea10c65c3
mac <mac@MacMini-KJS-1.local>
2025-09-02 10:58:17 +0700

    fix

--- t2i.py
+++ t2i.py
@@ -6,12 +6,20 @@ from rembg import remove, new_session
 # pipe = DiffusionPipeline.from_pretrained("Tencent-Hunyuan/HunyuanDiT-v1.1-Diffusers-Distilled").to("cuda")
 
     # Load the text-to-multi-view pipeline
-text_to_multi_view_pipeline = DiffusionPipeline.from_pretrained(
-    "ashawkey/mvdream-sd2.1-diffusers", # Or another suitable MVDream diffusers port
-    custom_pipeline="dylanebert/multi_view_diffusion",
-    torch_dtype=torch.float16,
-    trust_remote_code=True,
-).to("cuda") # Move to GPU if available
+# text_to_multi_view_pipeline = DiffusionPipeline.from_pretrained(
+#     "ashawkey/mvdream-sd2.1-diffusers", # Or another suitable MVDream diffusers port
+#     custom_pipeline="dylanebert/multi_view_diffusion",
+#     torch_dtype=torch.float16,
+#     trust_remote_code=True,
+# ).to("cuda") # Move to GPU if available
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+
+t2i_pipe = DiffusionPipeline.from_pretrained(
+    model_id, 
+    dtype=torch.float16
+).to("cuda")
+
 
 prompt = input()
 # image = pipe(prompt + ", white background, 3D style, best quality", negative_prompt="Text, close-up, cropped, out of frame, worst quality, low quality, JPEG artifacts, PGLY, repetitive, morbid," \
@@ -23,7 +31,7 @@ prompt = input()
 
 # output.save('output.png')
 # Generate images; adjust guidance_scale and num_inference_steps as needed
-multi_view_images = text_to_multi_view_pipeline(
+multi_view_images = t2i_pipe(
     prompt,
     guidance_scale=7.5, # Example value, adjust for desired output
     num_inference_steps=50, # Example value, adjust for desired quality/speed
