af5e87d93671b2530b42fa01256bad6c3c29a062
mini <mini@Macmini.lan>
2025-09-03 23:22:30 +0700

    fix

--- t2i23-test-hunyuan.py
+++ t2i23-test-hunyuan.py
@@ -28,7 +28,7 @@ t2i_pipe.transformer = t2i_pipe.transformer.half()
 t2i_pipe.vae = t2i_pipe.vae.half()
 t2i_pipe.text_encoder = t2i_pipe.text_encoder.half()
 
-i23_pipeline = nn.DataParallel(TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large"))
+i23_pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
 i23_pipeline.cuda()
 
 prompts_file = open("/workspace/logs/prompts.txt", "r")
