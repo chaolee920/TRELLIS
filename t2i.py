829d13b2c38364fd27ff99da18b0cd39b5d3b782
mini <mini@MacMini-RDR-1.local>
2025-09-03 10:39:33 +0700

    fix

--- t2i.py
+++ t2i.py
@@ -3,16 +3,6 @@ import torch
 from diffusers import DiffusionPipeline
 from rembg import remove, new_session
 
-# pipe = DiffusionPipeline.from_pretrained("Tencent-Hunyuan/HunyuanDiT-v1.1-Diffusers-Distilled").to("cuda")
-
-    # Load the text-to-multi-view pipeline
-# text_to_multi_view_pipeline = DiffusionPipeline.from_pretrained(
-#     "ashawkey/mvdream-sd2.1-diffusers", # Or another suitable MVDream diffusers port
-#     custom_pipeline="dylanebert/multi_view_diffusion",
-#     torch_dtype=torch.float16,
-#     trust_remote_code=True,
-# ).to("cuda") # Move to GPU if available
-
 model_id = "stabilityai/stable-diffusion-2"
 
 t2i_pipe = DiffusionPipeline.from_pretrained(
@@ -22,20 +12,14 @@ t2i_pipe = DiffusionPipeline.from_pretrained(
 
 
 prompt = input()
-# image = pipe(prompt + ", white background, 3D style, best quality", negative_prompt="Text, close-up, cropped, out of frame, worst quality, low quality, JPEG artifacts, PGLY, repetitive, morbid," \
-# "Mutilation, extra fingers, mutant hands, poorly drawn hands, poorly drawn faces, mutations, deformities, blurry, dehydrated, poor anatomy," \
-# "Bad proportions, extra limbs, cloned faces, disfigurement, disgusting proportions, deformed limbs, missing arms, missing legs," \
-# "Extra arms, extra legs, fused fingers, too many fingers, long neck", num_inference_steps=25, width=512, height=512,  guidance_scale=3.5).images[0]
 
-# output = remove(image, session=new_session(), bgcolor=[255, 255, 255, 0])
+image = t2i_pipe(
+    prompt + ", white background, 3d style, best quality, high resolution",
+    guidance_scale=15, # Example value, adjust for desired output
+    num_inference_steps=25, # Example value, adjust for desired quality/speed
+).images[0]
 
-# output.save('output.png')
-# Generate images; adjust guidance_scale and num_inference_steps as needed
-multi_view_images = t2i_pipe(
-    prompt,
-    guidance_scale=7.5, # Example value, adjust for desired output
-    num_inference_steps=50, # Example value, adjust for desired quality/speed
-).images
+rembg_session = new_session('u2net')
+image = remove(image, session=rembg_session)
 
-for i in range(len(multi_view_images)):
-    multi_view_images[i].save(f"./MVDREAM/img_{i}.png")
+image.save(f"./output.png")
