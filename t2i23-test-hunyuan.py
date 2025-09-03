ea19c05556732fcb73bcefab61581b53ef67bd64
mini <mini@Macmini.lan>
2025-09-03 22:53:38 +0700

    fix

--- t2i23-test-hunyuan.py
+++ t2i23-test-hunyuan.py
@@ -22,14 +22,14 @@ model_id = "Tencent-Hunyuan/HunyuanDiT-v1.2-Diffusers-Distilled"
 t2i_pipe = HunyuanDiTPipeline.from_pretrained(
     model_id, 
     dtype=torch.float16
-).to("cuda")
+).to("cuda:1")
 
 t2i_pipe.transformer = t2i_pipe.transformer.half()
 t2i_pipe.vae = t2i_pipe.vae.half()
 t2i_pipe.text_encoder = t2i_pipe.text_encoder.half()
 
 i23_pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
-i23_pipeline.cuda()
+i23_pipeline.cuda(2)
 
 prompts_file = open("/workspace/logs/prompts.txt", "r")
 cnt = 0
