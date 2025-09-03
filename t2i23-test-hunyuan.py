bdaf6f4431d227ffb64cb2a8d7d81dd45eba5afd
mini <mini@Macmini.lan>
2025-09-04 03:27:54 +0700

    fix

--- t2i23-test-hunyuan.py
+++ t2i23-test-hunyuan.py
@@ -22,9 +22,9 @@ t2i_pipe = HunyuanDiTPipeline.from_pretrained(
     dtype=torch.float16
 ).to("cuda:1")
 
-# t2i_pipe.transformer = t2i_pipe.transformer.to("cuda:1")
-# t2i_pipe.vae = t2i_pipe.vae.to("cuda:2")
-# t2i_pipe.text_encoder = t2i_pipe.text_encoder.to("cuda:2")
+t2i_pipe.transformer = t2i_pipe.transformer.half()
+t2i_pipe.vae = t2i_pipe.vae.half()
+t2i_pipe.text_encoder = t2i_pipe.text_encoder.half()
 
 i23_pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large").to("cuda:3")
 
