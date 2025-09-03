19c1f0f6a17d230d98fa3cfa0a4f00ba91d3aeb3
mac <mac@MacMini-KJS-1.local>
2025-09-04 03:33:12 +0700

    fix: change datasets name

--- decompress_spz.py
+++ decompress_spz.py
@@ -24,7 +24,7 @@ def decompress_spz_files(csv_path, output_dir):
     return df
 
 if __name__ == "__main__":
-    csv_path = "datasets/404mini/404mini_100.csv"
+    csv_path = "datasets/404mini/404mini.csv"
     output_dir = "datasets/404mini"
     try:
         df = decompress_spz_files(csv_path, output_dir)
