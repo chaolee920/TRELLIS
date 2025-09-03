74a916ca2a52bfe5e3b22ca0255856e898cbc9d0
mac <mac@MacMini-KJS-1.local>
2025-09-04 03:49:42 +0700

    fix: change datasets name

--- prepare_train.py
+++ prepare_train.py
@@ -27,7 +27,7 @@ def load_404mini_dataset(repo_id="404-Gen/404mini", output_dir="datasets/404mini
                 prompt = base_name.replace("_", " ")
                 
                 # Download .ply.spz
-                spz_path = os.path.join(spz_dir, f"{base_name}.ply.spz")
+                spz_path = os.path.join(spz_dir, "/assets/", f"{base_name}.ply.spz")
                 hf_hub_download(repo_id=repo_id, filename=ply_file, repo_type="dataset", local_dir=spz_dir)
                 
                 # Check for .png (optional)
