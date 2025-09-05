fa1affee4802018204216868c7200d8c6973e736
mac <mac@MacMini-KJS-1.local>
2025-09-06 04:08:23 +0700

    fix

--- dataset_toolkits/build_metadata.py
+++ dataset_toolkits/build_metadata.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from easydict import EasyDict as edict
 from concurrent.futures import ThreadPoolExecutor
 import utils3d
-
+sys.path.append("./datasets")
 def get_first_directory(path):  
     with os.scandir(path) as it:  
         for entry in it:  
