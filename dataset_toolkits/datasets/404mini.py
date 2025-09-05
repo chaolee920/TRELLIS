3b0a7ab7f2f834a3750369d2d2444af798c7d977
mac <mac@MacMini-KJS-1.local>
2025-09-06 04:30:54 +0700

    fix

--- dataset_toolkits/datasets/404mini.py
+++ dataset_toolkits/datasets/404mini.py
@@ -27,7 +27,7 @@ def foreach_instance(metadata, output_dir, func, max_workers=None, desc='Process
             tqdm(total=len(metadata), desc=desc) as pbar:
             def worker(metadatum):
                 try:
-                    local_path = metadatum['local_path']
+                    local_path = metadatum['file_identifier']
                     sha256 = metadatum['sha256']
                     file = os.path.join(output_dir, local_path)
                     record = func(file, sha256)
