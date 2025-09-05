59d1e56c976484416435d9856762c2fc81c93c91
mac <mac@MacMini-KJS-1.local>
2025-09-06 02:29:55 +0700

    finetuing fix

--- decompress_spz.py
+++ decompress_spz.py
@@ -4,21 +4,32 @@ import pandas as pd
 
 def decompress_spz_files(csv_path, output_dir):
     df = pd.read_csv(csv_path)
-    ply_dir = os.path.join(output_dir, "ply")
-    os.makedirs(ply_dir, exist_ok=True)
+    
+    
     
     for idx, row in df.iterrows():
+        file_dir = os.path.join(output_dir, "files", row['file_identifier'])
+        os.makedirs(file_dir, exist_ok=True)
         spz_path = row['model_path']
-        ply_path = os.path.join(ply_dir, f"{row['uid']}.ply")
+        render_path = row['render_path']
+        ply_path = os.path.join(file_dir, f"{row['uid']}.ply")
+        png_path = os.path.join(file_dir, f"{row['uid']}.png")
         try:
+            # Decompress spz and push to file_identifier directory
             with open(spz_path, 'rb') as f:
                 compressed = f.read()
             decompressed = pyspz.decompress(compressed, include_normals=True)
             with open(ply_path, 'wb') as f:
                 f.write(decompressed)
+            # push png to file_identifier directory
+            with open(render_path, 'rb') as f:
+                png = f.read()
+            with open(png_path, 'wb') as f:
+                f.write(png)
             df.at[idx, 'model_path'] = ply_path
+            df.at[idx, 'render_path'] = png_path
         except Exception as e:
-            print(f"Error decompressing {spz_path}: {e}")
+            print(f"Error Pushing {spz_path}, {render_path}: {e}")
     
     df.to_csv(csv_path, index=False)
     return df
