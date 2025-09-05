baa19bebd47ae505ded8fdd9f63f24d45a435ca1
mini <mini@Macmini.lan>
2025-09-05 17:01:50 +0700

    fix

--- t2i23-test-hunyuan.py
+++ t2i23-test-hunyuan.py
@@ -87,7 +87,7 @@ while cnt < 10 :
     torch.cuda.empty_cache()
     prompt = prompts_file.readline()
     t0 = time()
-    generate(prompt, guidance_scale=1.0)
+    generate(prompt, guidance_scale=3.0)
 
     # validation_score = validate()
     
