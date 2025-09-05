fa1204460287fc93412059034857cc8369d13834
mac <mac@MacMini-KJS-1.local>
2025-09-06 04:09:33 +0700

    fix

--- dataset_toolkits/datasets/404mini.py
+++ dataset_toolkits/datasets/404mini.py
@@ -2,7 +2,10 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 import pandas as pd
+import argparse
 
+def add_args(parser: argparse.ArgumentParser):
+    pass
 
 def get_metadata(**kwargs):
     metadata = pd.read_csv("/workspace/proj-sub17/TRELLIS/datasets/404mini/metadata.csv")
