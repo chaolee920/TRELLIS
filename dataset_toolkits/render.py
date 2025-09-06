7b1128d12229f1c40639518601a46719b08e366d
mac <mac@MacMini-KJS-1.local>
2025-09-06 15:03:06 +0700

    fix

--- dataset_toolkits/render.py
+++ dataset_toolkits/render.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     dataset_utils.add_args(parser)
     parser.add_argument('--rank', type=int, default=0)
     parser.add_argument('--world_size', type=int, default=1)
-    parser.add_argument('--max_workers', type=int, default=8)
+    parser.add_argument('--max_workers', type=int, default=30)
     opt = parser.parse_args(sys.argv[2:])
     opt = edict(vars(opt))
 
