931da2ee87fb8e5035b04c5c0bc5b6110d1cffac
mac <mac@MacMini-KJS-1.local>
2025-09-06 14:32:31 +0700

    fix

--- dataset_toolkits/render.py
+++ dataset_toolkits/render.py
@@ -51,9 +51,9 @@ def _render(file_path, sha256, output_dir, num_views):
     ]
     if file_path.endswith('.blend'):
         args.insert(1, file_path)
-    
+    print('stating blendling')
     call(args, stdout=DEVNULL, stderr=DEVNULL)
-    
+    print('ended blending')
     if os.path.exists(os.path.join(output_folder, 'transforms.json')):
         return {'sha256': sha256, 'rendered': True}
 
