f08e16ffba4ec117d8ce1e851221c830733525c7
mini <mini@Macmini.lan>
2025-09-08 23:49:50 +0700

    release: 2-gpu release

--- serve.py
+++ serve.py
@@ -194,19 +194,25 @@ async def generate(
     t0 = time()
 
     output_t23, score_t23 = generate_t23(prompt)
-    output_t2i23, score_t2i23 = generate_t2i23(prompt, guidance_scale=9.0, num_inference_steps=20)
-
-    if score_t23 >= score_t2i23:
+    if score_t2i23 > 0.65:
         best_score = score_t23
         best_gaussian = output_t23
     else:
-        best_score = score_t2i23
-        best_gaussian = output_t2i23
+        output_t2i23, score_t2i23 = generate_t2i23(prompt, guidance_scale=9.0, num_inference_steps=20)
+        if score_t23 < score_t2i23:
+            best_gaussian = output_t2i23
+            best_score = score_t2i23
+        else:
+            best_gaussian = output_t23
+            best_score = score_t23
     
     t1 = time()
     print(f"====Final Score: {best_score}, Generation took: {t1 - t0}====")
     logging.info(f"Final Score: {best_score}, Generation took: {t1 - t0}")
     logging.info("=============================================================")
+
+    if best_score < 0.6:
+        return None
 
     buffer = BytesIO()
     best_gaussian.save_ply(buffer)
